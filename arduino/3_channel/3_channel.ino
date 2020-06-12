4e26ac5acf1601f78c005941d1bf81434374ed85
yashaskull <arvid.ramdeane@my.uwi.edu>
2020-06-12 18:47:48 +0000

    changes to 3_channel.ino

--- arduino/3_channel/3_channel.ino
+++ arduino/3_channel/3_channel.ino
@@ -95,12 +95,12 @@ void setup() {
 
   Serial.begin(115200);
 
-  
+  /**
   while (!Serial.available())
   {
     delay(1000);
     // do nothing
-  }
+  }*/
 
   while(Serial.available())
   {
@@ -201,7 +201,12 @@ void readData(bool print2Serial)
   counter ++;
   if (print2Serial == true) 
   {
-    //Serial.println(bit24_N);
+ //   if (bit24_N >= 8388608 && bit24_N <= 16777215)
+  //  {
+    //  Serial.println((16777215 - bit24_N)*-1);
+  //  }
+   // else
+      Serial.println(bit24_N);
     /**
     Serial.print(bit24_N);
     Serial.print(" ");
@@ -209,7 +214,7 @@ void readData(bool print2Serial)
     Serial.print(" ");
     Serial.println(bit24_Z);*/
     //Serial.println((String)bit24_Z);
-   Serial.println((String)bit24_N+'*'+(String)bit24_E+'*'+(String)bit24_Z);
+   //Serial.println((String)bit24_N+'*'+(String)bit24_E+'*'+(String)bit24_Z);
   //Serial.println("1000*1000*1000");
   //Serial.println('z'+(String)bit24_Z+'*'+'n'+(String)bit24_N+'*'+'e'+(String)bit24_E);
     //Serial.println(bit24_E);
