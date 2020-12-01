dc10b21fbb632e2faeeb1b353a9a4531f38e5df4
yashaskull <arvid.ramdeane@my.uwi.edu>
2020-12-01 15:03:00 +0000

    hold button for 5 seconds before shutdown occures

--- scripts/power_switch/listen_for_shutdown.py
+++ scripts/power_switch/listen_for_shutdown.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-
+import time
 import datetime
 import RPi.GPIO as GPIO
 import subprocess
@@ -9,15 +9,36 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-GPIO.setup(23, GPIO.OUT)
-GPIO.output(23, GPIO.HIGH)
+loop_counter = 0
+#GPIO.wait_for_edge(3, GPIO.FALLING)
 
-GPIO.wait_for_edge(3, GPIO.FALLING)
+while True:
+	GPIO.wait_for_edge(3, GPIO.FALLING)
+	while True:
+		#print(loop_counter)
+		if loop_counter == 5: # 5 sec
+			date_time = datetime.datetime.now()
+			f = open("/home/arvid/projects/sdas/scripts/power_switch/shutdown.log", "w+")
+			f.write("Shutdown initiated via swtich at " +str(date_time))
+			f.close()
+			subprocess.call(['shutdown', '-h', 'now'], shell=False)
+			#print("shutdown initiated")
+			#loop_counter = 0
+			#break
+		time.sleep(1)
+		if GPIO.input(3) == 0:
+			loop_counter = loop_counter + 1
+			continue
+		else:
+			loop_counter = 0
+			break
 
+
+#print("shutdown initiated")
 #date_time = datetime.datetime.now()
 #f = open("/home/arvid/software/scripts/power_switch/shutdown.log", "w+")
 #f.write("Shutdown initiated via swtich at " +str(date_time))
 
-GPIO.output(23, GPIO.LOW)
+#GPIO.output(23, GPIO.LOW)
 
 #subprocess.call(['shutdown', '-h', 'now'], shell=False)
